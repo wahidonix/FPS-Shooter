74ef862d1b7e1dd85aa007f5f466ee4c17de5641
wahidonix <wahidonix@gmail.com>
2023-06-27 23:05:40 +0200

    - Implemented shooting mechanics - Implemented enemies with path finding
    see #3

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody3D
 
 var speed
-const WALK_SPEED = 5.0
-const SPRINT_SPEED = 8.0
+const WALK_SPEED = 6.0
+const SPRINT_SPEED = 9.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.004
+const HIT_STAGGER = 8.0
 
 #bob variables
 const BOB_FREQ = 2.4
@@ -15,11 +16,23 @@ var t_bob = 0.0
 const BASE_FOV = 75.0
 const FOV_CHANGE = 1.5
 
+# signal
+signal player_hit
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 9.8
 
+# Bullets
+var bullet = load("res://Scenes/Bullet.tscn")
+var instance
+
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
+@onready var gun_anim = $Head/Camera3D/Rifle/AnimationPlayer
+@onready var gun_barrel = $Head/Camera3D/Rifle/RayCast3D
+@onready var gun_anim2 = $Head/Camera3D/Rifle2/AnimationPlayer
+@onready var gun_barrel2 = $Head/Camera3D/Rifle2/RayCast3D
+
 
 
 func _ready():
@@ -49,12 +62,12 @@ func _physics_process(delta):
 		speed = WALK_SPEED
 
 	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
-			velocity.x = direction.x * speed
-			velocity.z = direction.z * speed
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 15.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 15.0)
 		else:
 			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
 			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
@@ -71,6 +84,22 @@ func _physics_process(delta):
 	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
 	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 	
+	# Shooting
+	if Input.is_action_pressed("shoot"):
+		if !gun_anim.is_playing():
+			gun_anim.play("Shoot")
+			instance = bullet.instantiate()
+			instance.position = gun_barrel.global_position
+			instance.transform.basis = gun_barrel.global_transform.basis
+			get_parent().add_child(instance)
+		if !gun_anim2.is_playing():
+			gun_anim2.play("Shoot")
+			instance = bullet.instantiate()
+			instance.position = gun_barrel2.global_position
+			instance.transform.basis = gun_barrel2.global_transform.basis
+			get_parent().add_child(instance)
+		
+	
 	move_and_slide()
 
 
@@ -79,3 +108,10 @@ func _headbob(time) -> Vector3:
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
 	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
 	return pos
+
+
+func hit(dir):
+	emit_signal("player_hit")
+	velocity += dir * HIT_STAGGER
+
+
